ba50866de1f495a536bdf94c2cae6f0957c85d32
WpEmbraced <45814109+WpEmbraced@users.noreply.github.com>
2023-09-03 18:13:32 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
 function wpe_add_google_fonts() {
-	wp_enqueue_style( 'wpb-google-fonts', '/wp-content/uploads/2023/09/CrimsonText-Bold.ttf', array(), null );
+	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap', array(), null );
 }
 add_action( 'wp_enqueue_scripts', 'wpe_add_google_fonts' );
